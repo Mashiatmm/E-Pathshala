3fb294f06a34bc36f7e1a68bfa602262e3278257
Naeem Ahmed <1705014@ugrad.cse.buet.ac.bd>
2020-04-30 23:06:43 +0600

    homepage temporary , did the settings, app added for accounts

--- E_Pathshala/urls.py
+++ E_Pathshala/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+import accounts.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',accounts.views.home,name='home'),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
