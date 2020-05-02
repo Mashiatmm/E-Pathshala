a603b1fb7b75e3eb072647f9dce16bece4c04139
Naeem Ahmed <1705014@ugrad.cse.buet.ac.bd>
2020-05-03 00:13:46 +0600

    login + signup done without specific-> (as teacher or student)

--- accounts/views.py
+++ accounts/views.py
@@ -15,15 +15,56 @@ def signup(request):
                 return render(request,'accounts/signup.html',{'error':"Email already taken!"})
 
             except User.DoesNotExist:
-                user=User.objects.create_user(username=request.POST['username'],email=request.POST['email'],password=request.POST['password2'])
-                auth.login(request,user)
-                return redirect('home')#redirecting to a url
-
-        
+                try:
+                    user=User.objects.get(username=request.POST['username'])
+                    return render(request,'accounts/signup.html',{'error':"Username already taken!"})
+                except User.DoesNotExist:
+                    user=User.objects.create_user(username=request.POST['username'],email=request.POST['email'],password=request.POST['password2'])
+                    auth.login(request,user)
+                    return redirect('home')#redirecting to a url                 
         else:
             return render(request,'accounts/signup.html',{'error':"Passwords didn't match!"})
     
     else:
         return render(request,'accounts/signup.html')
 
+
+def login(request):
+    if request.method=='POST':
+        try:
+            user=User.objects.get(email=request.POST['email'])
+            
+        except User.DoesNotExist:
+            try:
+                user=User.objects.get(username=request.POST['email'])  
+                   
+            except User.DoesNotExist:
+                return render(request,'accounts/login.html',{'error':"User Does Not Exist!!!"})
+        if check_password(request.POST['password'], user.password):
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'accounts/login.html',{'error':"Password didn't match!"})
+       
+            
+
+    else:
+        return render(request,'accounts/login.html')
+
+
     
+
+
+    
+'''
+if request.method=='POST':#if inside the page anything is posted ie name and password
+        user=auth.authenticate(email=request.POST['email'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else :
+            return render(request,'accounts/login.html',{'error':'email or password is wrong!'})
+               
+    else:#if a get request ie the page is requested via the url
+        return render(request,'accounts/login.html')
+'''
