229f01fdd78d57448ac7c15763246203b12c6462
Naeem Ahmed <1705014@ugrad.cse.buet.ac.bd>
2020-05-02 20:51:04 +0600

    signup done

--- E_Pathshala/urls.py
+++ E_Pathshala/urls.py
@@ -1,14 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 import accounts.views
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',accounts.views.home,name='home'),
+    path('accounts/',include('accounts.urls')),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from django.contrib import auth
 
 def home(request):
-    return render(request,'accounts/home.html')
+    return render(request,'accounts/home.html')\
+
+
+def signup(request):
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user=User.objects.get(email=request.POST['email'])
+                return render(request,'accounts/signup.html',{'error':"Email already taken!"})
+
+            except User.DoesNotExist:
+                user=User.objects.create_user(username=request.POST['username'],email=request.POST['email'],password=request.POST['password2'])
+                auth.login(request,user)
+                return redirect('home')#redirecting to a url
+
+        
+        else:
+            return render(request,'accounts/signup.html',{'error':"Passwords didn't match!"})
+    
+    else:
+        return render(request,'accounts/signup.html')
+
+    
