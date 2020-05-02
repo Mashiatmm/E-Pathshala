b3624d19bfad5e379803f525c45e5bdffc811076
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2020-05-02 13:48:32 +0600

    Student, Teacher Create

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Student, Teacher
 # Register your models here.
+
+myModels = [Student,Teacher]  # iterable list
+admin.site.register(myModels)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Student(models.Model):
+    GRADE = [
+     ("1", "1"), 
+    ("2", "2"), 
+    ("3", "3"), 
+    ("4", "4"), 
+    ("5", "5"), 
+    ("6", "6"), 
+    ("7", "7"), 
+    ("8", "8"), 
+    ("9", "9"), 
+    ("10", "10"), 
+    ("11", "11"), 
+    ("12", "12"), 
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #Class can be imported from Grade entity 
+    grade = models.CharField( max_length=20, choices=GRADE,default='1')
+
+
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Specialty = models.CharField(max_length=200)
+    
