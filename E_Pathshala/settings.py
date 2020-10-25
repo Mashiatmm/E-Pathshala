27a340f1d708bc8b74e8d5523343e6a3719362fe
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2020-10-26 02:00:02 +0600

    Database connected , migration not working

--- E_Pathshala/settings.py
+++ E_Pathshala/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'E_Pathshala.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'ORCL',
+        'USER' : 'EPATHSHALA',
+        'PASSWORD': '123',
+        'HOST' : 'localhost',
+        'PORT' : '1521'
     }
 }
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 from django.contrib import auth
+from django.db import connection
 import cx_Oracle
+
+#Mashiat Virtual Env - 'myvenv'
 
 def home(request):
     return render(request,'accounts/home.html')
 
 
 def signup(request,role):
+    '''
     dsn_tns  = cx_Oracle.makedsn('localhost','1521',service_name='ORCL')
     conn = cx_Oracle.connect(user='hr',password='hr',dsn=dsn_tns)
-    c = conn.cursor()
+    '''
+    c = connection.cursor()
 
     if request.method=='POST':
-        print('Received\n')
+        
         
     
         if request.POST['password1']==request.POST['password2']:
@@ -30,53 +34,58 @@ def signup(request,role):
                 name_exists = c.fetchall()
 
                 if name_exists == []:
-
-                    count=conn.cursor()
-                    count.execute("select count(*) from USERS")
-                    val,=count.fetchone()
-                    statement = 'insert into USERS(id,name,email, password,role) values (:1,:2, :3, :4,:5)'
-                    c.execute(statement, (val, request.POST['username'], request.POST['email'],request.POST['password2'],role))
                     
+                    print(request.POST['username'], request.POST['email'],request.POST['password2'],role)
+                    statement = 'insert into USERS(name,email, password,role) values (%s,%s, %s,%s)'
+                    c.execute(statement, (request.POST['username'], request.POST['email'],request.POST['password2'],role))
+                    
+                    count=connection.cursor()
+                    count.execute("select id from USERS where email = :usermail",{'usermail':request.POST['email']})
+                    val,=count.fetchone()
+                    count.close()
+
                     
                     if role== 'student':
-                        statement='insert into STUDENT(id,grade) values (:1,:2)'
+                        statement='insert into STUDENTS(id,class) values (%s,%s)'
                         c.execute(statement,(val,request.POST['grade']))
                     
                     else:
-                        statement='insert into TEACHER(id,specialty) values (:1,:2)'
+                        statement='insert into TEACHERS(id,specialty) values (%s,%s)'
                         c.execute(statement,(val,request.POST['specialty']))
                     
                     
-                    conn.commit()
-                    conn.close()
+                    
+                    connection.commit()
+                    c.close()
+
                     
                     return render(request,'accounts/profile.html',{'role':role,'name':request.POST['username'],'email':request.POST['email'],'password':request.POST['password2']})
                 else:
-                    conn.close()
-                    return render(request,'accounts/signup.html',{'error':"Username already taken!"})
+                    c.close()
+                    return render(request,'accounts/signup.html',{'role':role,'error':"Username already taken!"})
 
 
 
             else:    
-                conn.close()
-                return render(request,'accounts/signup.html',{'error':"Email already taken!"})
+                c.close()
+                return render(request,'accounts/signup.html',{'role':role,'error':"Email already taken!"})
         
         else:
 
-            return render(request,'accounts/signup.html',{'error':"Passwords didn't match!"})
+            return render(request,'accounts/signup.html',{'role':role,'error':"Passwords didn't match!"})
     
     else:
         if role == 'student':
-            conn.close()
+            c.close()
             return render(request,'accounts/signup.html',{'role':role})
         else:
-            statement='select distinct specialty from TEACHER'
+            statement='select distinct specialty from TEACHERS'
             c.execute(statement)
             all_spec=c.fetchall()
             all_specialties=[]
             for spec in all_spec:
                 all_specialties.append(spec[0])
-            conn.close()
+            c.close()
             return render(request,'accounts/signup.html',{
                 'role':role,
                 'all_specialties':all_specialties
@@ -87,18 +96,20 @@ def signup(request,role):
 
 def login(request):
     if request.method=='POST':
+        '''
         dsn_tns  = cx_Oracle.makedsn('localhost','1521',service_name='ORCL')
         conn = cx_Oracle.connect(user='hr',password='hr',dsn=dsn_tns)
-        c = conn.cursor()
-        email_or_name= request.POST['email_or_name']
+        '''
+        c = connection.cursor()
+        email= request.POST['email_or_name']
         password=request.POST['password']
-        statement= 'select name from USERS where email=:username or name=:username'
-        c.execute(statement,{'username':email_or_name})
-        user_exists,=c.fetchone()
+        statement= 'select name from USERS where email=:username'
+        c.execute(statement,{'username':email})
+        user_exists=c.fetchall()
 
         if user_exists == [] :
-            conn.close()
-            return render(request,'accounts/login.html',{'error':"User Does Not Exist!!!"})
+            c.close()
+            return render(request,'accounts/login.html',{'error':"Mail ID Does Not Exist!!!"})
 
         else:
             statement="select password,role,id from USERS where name=:username"
@@ -106,11 +117,11 @@ def login(request):
             info= c.fetchone()
         
             if(password==info[0]):
-                conn.close()
+                c.close()
                 return redirect('/accounts/'+info[1]+'/'+str(info[2]))
 
             else:
-                conn.close()
+                c.close()
                 return render(request,'accounts/login.html',{'error':"Incorrect Password!!"})
                 
     
@@ -120,27 +131,25 @@ def login(request):
 
 
 def profile(request,role,id):
-    
+    '''
     dsn_tns  = cx_Oracle.makedsn('localhost','1521',service_name='ORCL')
-    conn = cx_Oracle.connect(user='hr',password='hr',dsn=dsn_tns)
-    c = conn.cursor()
+    c = cx_Oracle.connect(user='hr',password='hr',dsn=dsn_tns)
+    '''
+    c = connection.cursor()
     print(role,id)
     statement=""
     if role=="student":
-        statement="""Select USERS.ID AS "ID",NAME,EMAIL,PASSWORD,GRADE 
-                    FROM USERS 
-                    JOIN STUDENT 
-                    on STUDENT.ID=USERS.ID 
-                    where USERS.ID=:userid"""
+        statement="""Select U.ID AS "ID",U.NAME,U.EMAIL,U.PASSWORD,S.CLASS 
+                    FROM USERS U, STUDENTS S 
+                    WHERE S.ID=U.ID AND U.ID=:userid"""
     else:
-        statement="""Select USERS.ID AS "ID",NAME,EMAIL,PASSWORD,Specialty 
-                    FROM USERS 
-                    JOIN TEACHER 
-                    on TEACHER.ID=USERS.ID 
-                    where USERS.ID=:userid"""
+        statement="""Select  U.ID AS "ID",U.NAME,U.EMAIL,U.PASSWORD,T.Specialty 
+                    FROM USERS U, TEACHERS T
+                    WHERE T.ID=U.ID AND U.ID=:userid"""
 
     c.execute(statement,{'userid':id})
     user,=c.fetchall()
+    c.close()
     return render(request,'accounts/profile.html',{'role':role,'name':user[1],'email':user[2],'password':user[3]})
 
 
