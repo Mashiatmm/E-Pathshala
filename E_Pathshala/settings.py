2b6a2638f971bfe0b4148d7ef872e7842b8c5b9d
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2020-11-22 00:27:10 +0600

    Forum app

--- E_Pathshala/settings.py
+++ E_Pathshala/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
     'accounts.apps.AccountsConfig',
-    'forum.apps.AccountsConfig',
+    'forum.apps.ForumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
