7e006ea6fe28e7e5431a7d5c325ce911912097fc
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2020-11-22 00:22:36 +0600

    Forum app added

--- E_Pathshala/urls.py
+++ E_Pathshala/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('',accounts.views.home,name='home'),
     path('accounts/',include('accounts.urls')),
     path('courses/',include('courses.urls')),
+    path('forum/',include('forum.urls')),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
